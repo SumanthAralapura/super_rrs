8fb15808ca806f6692518c65f8923925d936cff2
sumanth <sumanth@zendynamix.com>
2022-02-15 13:40:30 +0530

    set up

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 import cv2
 from numpy import asarray
 from datetime import datetime
-print(cv2.__version__)
 
-
-print(__name__)
+print('cv2 version', cv2.__version__)
 
 
 def espcn(np_img):
@@ -16,37 +14,37 @@ def espcn(np_img):
     cv2.imwrite('output/test2_ESPCN_x4' + datetime.now() + '.jpg', result)
 
 
-def edsr():
+def edsr(np_img):
     try:
         sr = cv2.dnn_superres.DnnSuperResImpl_create()
         path = "EDSR_x4.pb"
         sr.readModel(path)
         sr.setModel("edsr", 4)
-        result = sr.upsample(img)
+        result = sr.upsample(np_img)
         cv2.imwrite('output/test_EDSR_x4' + datetime.now() + '.jpg', result)
     except:
         print('error in edsr')
 
 
-def fsrcnn():
+def fsrcnn(np_img):
     try:
         sr = cv2.dnn_superres.DnnSuperResImpl_create()
         path = "FSRCNN_x4.pb"
         sr.readModel(path)
         sr.setModel("fsrcnn", 3)
-        result = sr.upsample(img)
+        result = sr.upsample(np_img)
         cv2.imwrite('output/test_FSRCNN_x4' + datetime.now() + '.jpg', result)
     except:
         print('fsrcnn error')
 
 
-def lapsrn():
+def lapsrn(np_img):
     try:
         sr = cv2.dnn_superres.DnnSuperResImpl_create()
         path = "LapSRN_x8.pb"
         sr.readModel(path)
         sr.setModel("lapsrn", 8)
-        result = sr.upsample(img)
+        result = sr.upsample(np_img)
         cv2.imwrite('output/test_LapSRN_x8' + datetime.now() + '.jpg', result)
     except:
         print('error in lapSRN')
